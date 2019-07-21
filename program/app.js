f60a83cf4fc7bc844c47451c233237653e99c48c
ThreeG <gaogongge@sina.com.cn>
2019-07-21 18:00:13 +0800

    lesson-3

--- program/app.js
+++ program/app.js
@@ -21,7 +21,10 @@ new Vue({
         updateXY:function(event){
             this.x=event.offsetX;
             this.y=event.offsetY;
+        },
+        stopMoving:function(event){
+             event.stopPropagation();
+             event.stopPropagation();
         }
-
     }
 })
