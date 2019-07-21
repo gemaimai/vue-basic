ffa26b3facedaaa6801e70be60dfde39952783d2
ThreeG <gaogongge@sina.com.cn>
2019-07-22 00:07:54 +0800

    lesson-11 v-if  & v-show

--- program/app.js
+++ program/app.js
@@ -2,25 +2,15 @@ var count=0;
 new Vue({
     el:"#app",
     data:{
-        changeColor:true,
-        changeLength:false
+        isShow:false,
+        isShowElse:false,
+        isShowElseIf:false,
+        isShow_v:false
     },
     methods:{
-        changeColorFn:function(){
-            this.changeColor=!this.changeColor;
-            this.changeLength=!this.changeLength;
-        }
+       
     },
     computed:{
-        getClassList:function(){
-
-            return {
-                changeColor:this.changeColor,
-                changeLength:this.changeLength
-            }
-        },
-        changeColorComputed:function(){
-            this.changeColor=!this.changeColor;
-        }
+       
     }
 })
